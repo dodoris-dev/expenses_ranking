16d9c753497a3d3a9a23ec672eb3b4bbea5f3274
dodoris-dev <idmoncap@gmail.com>
2024-09-22 23:10:52 -0300

    refactor: update routes.rb to add new routes for data processing and deputy expenses

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "data/processing_csv", to: "data#processing_csv"
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  resources :expenses
+  resources :deputies do
+    get "expenses_sum", to: "deputies#return_expenses_sum", on: :member
+    get "highest_expense", to: "deputies#return_highest_expense", on: :member
+    get "list_expenses", to: "deputies#list_expenses", on: :member
+  end
+
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  match "*path", to: "application#render_not_found", via: :all
 end
